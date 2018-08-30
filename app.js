e8f8de58dbdab481c0803c59bfd9d757f44e9177
Matt <matthew.wong@outlook.com>
2018-08-30 12:37:44 -0400

    User registration and checks completed

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 const express = require('express')
 
 const registerUser = require('./router/register.js')
+const loginUser = require('./router/login.js')
 const path = require('path')
+var bodyParser = require('body-parser')
+
 var engine = require('consolidate');
 
 
 const app = express()
 
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 app.set('views', __dirname + '/views');
 app.engine('html', engine.mustache);
 app.set('view engine', 'html');
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.use('/register', registerUser);
+app.use('/login', loginUser);
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
--- router/register.js
+++ router/register.js
@@ -3,9 +3,80 @@ const router = express.Router();
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('db/mydb.db');
 
+
+let checkUser = (username, callback) => {
+    db.get(`SELECT username from users WHERE username = ?`, [username], function(err, rows) {
+        console.log(rows);
+
+        if(rows == undefined){
+          callback(false);
+        }
+        else{
+          callback(true);
+        }
+
+
+
+    });
+}
+/*
+addUser will be checking and adding the username in the Database
+if the user exists then don't add and return false
+if the user does not exist just execute the query and return true
+
+the return value reflects what will be displayed after the POST request
+*/
+let addUser = (username, callback) => {
+    checkUser(username, function(callbackCheck) {
+        // User does not exist
+
+        if (callbackCheck == false) {
+            db.run(`INSERT into users(username) VALUES (?)`, [username], function(err) {
+                if (err) {
+                    console.log(err);
+                }
+                // get the last insert id
+                console.log(`A row has been inserted with rowid ${this.lastID}`);
+            });
+
+            callback(true); // Query ran well
+        } else {
+            callback(false); // Ask the user for a different name
+        }
+    });
+
+
+
+}
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('register.html');
+    res.render('register.html',{
+      result:null
+    });
+});
+
+
+router.post('/', function(req, res, next) {
+    // Grab the username from POST data
+    const username = req.body.username;
+    //console.log(username);
+    const result = addUser(username, function(callback) {
+        var message;
+        console.log(username);
+        if (callback == true) {
+            message = "User has been registered";
+        } else {
+            message = `Pick another username. The username "${username}" already exists`;
+        }
+        const displayResult = callback ? "true" : "false";
+
+        res.render("register.html", {
+            result: message
+        });
+    });
+
+
 });
 
 module.exports = router;
