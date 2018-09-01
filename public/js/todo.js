5cb9f742ea21abb7770239d4accdfbee9dcd8b62
Matt <matthew.wong@outlook.com>
2018-09-01 02:43:36 -0400

    Completed with todo add api. Todo item submissions are updated on client and server side.

--- public/js/todo.js
+++ public/js/todo.js
@@ -1,33 +1,46 @@
 $( document ).ready(function() {
     $("#submitItem").on("click",function(){
-      const todoItem = $("#addItem").val();
-      const uuid = "94e7ee84-b1d2-46a9-a9e3-eefa3d8be441";
+      const todoItem = encodeURIComponent($("#addItem").val());
+      const uuid = " fake uuid 94e7ee84-b1d2-46a9-a9e3-eefa3d8be441";
       const rowTemplate = `
 
       <tr>
+      <td>
+        <div class="centered">
+          <input type="checkbox" checked="true"/>
+        </div>
+
+      </td>
 
         <td>`+todoItem+`</td>
-        <td>
+        <td class="centered">
 
-          Open
+          open
         </td>
-        <td><input type="hidden" value="`+uuid+`" /></td>
+        <td><div class="delete centered">
+          <button>x</button>
+        </div><input type="hidden" value="{{{ username }}}!{{{ @index }}}" /></td>
 
-        <td>
-
-          <select name="status">
-            <option>-</option>
-            <option>Open</option>
-            <option>Done</option>
-            <option>Remove</option>
-          </select>
-        </td>
       </tr>
 
 
       `
       $("#addItem").val("");
       $("#todoList").append(rowTemplate);
+
+      const data = {
+        "description":todoItem
+      }
+
+      $.ajax({
+    url: '/api/add',
+    type: "POST",
+    data: JSON.stringify(data),
+    contentType: "application/json",
+
+},function(data){
+
+});
       console.log("click");
     });
 });
--- router/api/add.js
+++ router/api/add.js
@@ -2,12 +2,33 @@ const express = require('express');
 const router = express.Router();
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('db/mydb.db');
+const session = require('express-session')
+const uuidv4 = require('uuid/v4')
 
 router.get('/', function(req, res, next) {
-  res.send("Improper method");
+  const result = {
+    "message":"Improper method"
+  }
+  res.json(result);
 });
 
 router.post('/', function(req, res, next) {
 
+  const result = {
+    "message":""
+  }
+  const username =req.session.username;
+  if(username === undefined){
+    result.message = "Not authorized"
+    res.json(result);
+  }
+
+  const item = req.body.description;
+  db.run(`INSERT INTO todo(username, description, status, uuid)
+  VALUES((?),(?),(?),(?))
+  `,[username, item, "open",uuidv4()]);
+  console.log(item);
+  result.message = "done";
+  res.json(result);
 });
 module.exports = router;
